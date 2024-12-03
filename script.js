3bbac7794588ec8e840c174b4733398b70bd9fd9
DaniPoo19 <vnegretebaza24@correo.unicordoba.edu.co>
2024-12-02 23:38:16 -0500

    Actualizacion visual y funcionalidad

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const API_BASE_URL = "http://127.0.0.1:5000"; // URL local para el backend
+    const API_BASE_URL = "citasmedicaschatbot-production.up.railway.app"; 
 
     // Botones de navegación
     const registerBtn = document.getElementById("view-register-btn");
